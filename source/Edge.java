9cc75f44fbd1911b8ab65e6dc6f9128c10d7ce2e
AlexCharidis <alexandroscharidis@gmail.com>
2017-04-08 14:39:41 -0400

    init commit

---
+++ source/Edge.java
@@ -0,0 +1,20 @@
+/*************************************************************************
+ *  Dependencies: None
+ *
+ *  Generic Edge data structure holding two variables for start and end
+ *  
+ *  @author Alexandros Charidis, MIT
+ *************************************************************************/
+
+public class Edge<Type> {
+    
+	public Type from, to;
+	
+	public Edge(Type from_, Type to_){
+		from = from_;
+		to = to_;
+	}
+	
+	public static void main(String[] args) { }
+
+}
